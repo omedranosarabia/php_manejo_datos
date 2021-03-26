408ed36973a191a03222bf8f1ce631830302661e
Omar Medrano <omar.medrano.ms@gmail.com>
2021-03-25 18:03:23 -0600

    Closure y funciones anónimas

--- index.php
+++ index.php
@@ -1,11 +1,54 @@
 <?php
 
-function greet()
+// Función anónima
+// Se usa en una variable que requiere lógica
+
+use PhpParser\Node\Expr\ClosureUse;
+
+$greet = function ($name) {
+    return "Hola, $name";
+};
+
+echo $greet('Omar');
+
+function greet2(Closure $lang, $name)
 {
-    //return ['PDF', 'vista'];
+
+    return $lang($name);
 }
 
-var_dump(greet());
+$es = function ($name) {
 
-exit(); // detiene el sistema
-return; // retorna
+    return "Hola, $name";
+};
+
+$en = function ($name) {
+    return "Hi, $name";
+};
+
+echo '<br>';
+echo greet2($es, 'Matic');
+echo '<br>';
+echo greet2($en, 'Tyrell');
+echo '<br>';
+
+function dateSeason(Closure $season, $date)
+{
+    return $season($date);
+}
+
+$spring = function($day){
+    
+    return "Hoy es $day y es Primavera";
+};
+
+$summer = function($day){
+    
+    return "Hoy es $day y es Verano";
+};
+
+$day = date("d-M-Y");
+
+echo dateSeason($spring, $day);
+echo '<br>';
+echo dateSeason($summer, $day);
