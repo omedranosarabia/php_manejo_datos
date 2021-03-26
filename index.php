c4804406435b927e1d548c30ec350032a36d46d6
Omar Medrano <omar.medrano.ms@gmail.com>
2021-03-25 18:35:06 -0600

    Array simple

--- index.php
+++ index.php
@@ -1,54 +1,8 @@
 <?php
+// Array simple
+$courses = ['javascript', 'laravel', 'php', 'vuejs'];
+var_dump($courses);
 
-// Función anónima
-// Se usa en una variable que requiere lógica
-
-use PhpParser\Node\Expr\ClosureUse;
-
-$greet = function ($name) {
-    return "Hola, $name";
-};
-
-echo $greet('Omar');
-
-function greet2(Closure $lang, $name)
-{
-
-    return $lang($name);
-}
-
-$es = function ($name) {
-
-    return "Hola, $name";
-};
-
-$en = function ($name) {
-    return "Hi, $name";
-};
-
-echo '<br>';
-echo greet2($es, 'Matic');
-echo '<br>';
-echo greet2($en, 'Tyrell');
-echo '<br>';
-
-function dateSeason(Closure $season, $date)
-{
-    return $season($date);
-}
-
-$spring = function($day){
-    
-    return "Hoy es $day y es Primavera";
-};
-
-$summer = function($day){
-    
-    return "Hoy es $day y es Verano";
-};
-
-$day = date("d-M-Y");
-
-echo dateSeason($spring, $day);
-echo '<br>';
-echo dateSeason($summer, $day);
+// Asignar valor a una posición epsecífica
+$courses = ['javascript', 'laravel', 10=> 'php', 'vuejs'];
+var_dump($courses);
