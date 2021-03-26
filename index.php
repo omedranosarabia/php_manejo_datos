2cdd2a65ab9aef6168da26476287959613dad1f5
Omar Medrano <omar.medrano.ms@gmail.com>
2021-03-25 19:05:46 -0600

    Arrays Complejos

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
 // Array simple
-$courses = ['javascript', 'laravel', 'php', 'vuejs'];
+$courses = [
+    'frontend' => 'javascript',
+    'framework' => 'laravel',
+    'backend' => 'php'
+];
+
 var_dump($courses);
 
-// Asignar valor a una posición epsecífica
-$courses = ['javascript', 'laravel', 10=> 'php', 'vuejs'];
-var_dump($courses);
+
+foreach ($courses as $key => $value) {
+    echo "$key: $value <br>";
+}
+
+echo '<br>';
+
+foreach ($courses as $course) {
+    echo "$course <br>";
+}
+
+function upper($course, $key)
+{
+    echo strtoupper($course) . ":" . strtoupper($key) . "<br>";
+}
+
+array_walk($courses, 'upper');
+
+// A nivel llave
+echo array_key_exists('frontend', $courses);
+echo '<br>';
+// A nivel valores
+echo in_array('javascript', $courses);
+echo '<br>';
+// Imprimir keys
+echo var_dump(array_keys($courses));
+// Imprimir valroes
+echo var_dump(array_values($courses));
